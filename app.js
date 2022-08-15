febbfdcf9a232fe2d05af3bb5a9896275240ad31
Sophie <sandovalsofi99@gmail.com>
2022-08-15 19:13:32 -0300

    Uploading everything i've been working on so far

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/home', (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+
+app.post('/crear',(req,res)=>{
+    res.send("Recibí un formulario")
+})
+
+app.listen(3030, ()=> console.log("El servidor está corriendo"))
